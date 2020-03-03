025fc4266382295c6a8ffba1f84d1c5ab3d1ddbf
Five Grant <5@fivegrant.com>
2020-03-02 21:25:25 -0500

    I literally lost all of my work so now I'm going make sure I commit a lot

---
+++ week2/linked_list.hpp
@@ -0,0 +1,29 @@
+#include <vector>
+// Linked List Class
+
+template <class T>
+class Link{
+  private:
+    T value; 
+    T *next; 
+  public: 
+    Link() {};
+    Link(T element): value(element){};
+    Link(T element, T *address): value(element),next(address){};
+    void insertAtHead(T element);
+    void insertAtTail(T element);
+    void insertAt(int index, T element);
+    void insertAt(int index, T element, int current);
+    void removeFromHead();
+    void removeFromTail();
+    void removeFrom(int index);
+    int length();
+    int length(int current);
+    int search(T element) const;
+    int search(T element, int current) const;
+    std::vector<int> searchAll(T element) const;
+    std::vector<int> searchAll(T element, int current, std::vector<int> collection) const;
+    std::vector<T> toArray() const;
+    std::vector<T> toArray(std::vector<T> collection) const;
+    void append(Link *links);
+};
