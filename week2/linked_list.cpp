0a1738f5078c2912a065efc8ae543d3e4ebc169a
Five Grant <5@fivegrant.com>
2020-03-02 21:33:38 -0500

    Update Week2 stuff incrementally so stuff isn't lost this time

---
+++ week2/linked_list.cpp
@@ -0,0 +1,80 @@
+void insertAtHead(T element)
+{
+
+}
+
+void insertAtTail(T element)
+{
+
+}
+
+void insertAt(int index, T element)
+{
+
+}
+
+void insertAt(int index, T element, int current)
+{
+
+}
+
+void removeFromHead()
+{
+
+}
+
+void removeFromTail()
+{
+
+}
+
+void removeFrom(int index)
+{
+
+}
+
+int length()
+{
+
+}
+
+int length(int current)
+{
+
+}
+
+int search(T element) const
+{
+
+}
+
+int search(T element, int current) const
+{
+
+}
+
+std::vector<int> searchAll(T element) const
+{
+
+}
+
+std::vector<int> searchAll(T element, int current, std::vector<int> collection) const
+{
+
+}
+
+std::vector<T> toArray() const
+{
+
+}
+
+std::vector<T> toArray(std::vector<T> collection) const
+{
+
+}
+
+void append(Link *links)
+{
+
+}
+
