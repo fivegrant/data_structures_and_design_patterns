ce560e1367815a586870714170498d379b1050a2
Five Grant <5@fivegrant.com>
2020-03-04 01:50:22 -0500

    Pass all tests with header only implementation

--- week2/test.cpp
+++ week2/test.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2.hpp"
 #include <string>
-#include "linked_combined.hpp"
+#include "linked_list_fixed.hpp"
 
 TEST_CASE("Initialize Linked List"){
    Link<std::string> first_node = Link<std::string>("Hello");
@@ -19,10 +19,44 @@ TEST_CASE("Insert into Linked List"){
    linked.insertAtTail("Third");
    linked.insertAtTail("Fourth");
    linked.insertAt(2, "Fifth");
-   std::vector<std::string> comp = {"First","Second","Fifth","Third", "Fourth"};
+   std::vector<std::string> comp = {"Second","Fifth","First","Third", "Fourth"};
    int size = linked.length();
-   REQUIRE(size == 4);
    REQUIRE(comp == linked.toArray());
+   REQUIRE(size == 5);
+   Link<std::string> linked2 = Link<std::string>("Extra");
+   linked2.insertAtTail("Stuff");
+   linked.append(&linked2);
+   std::vector<std::string> comp2 = {"Second","Fifth","First","Third", "Fourth", "Extra", "Stuff"};
+   REQUIRE(comp2 == linked.toArray());
+}
+
+TEST_CASE("Remove Elements"){
+   Link<std::string> linked = Link<std::string>("First");
+   linked.insertAtHead("Second");
+   linked.insertAtTail("Third");
+   linked.insertAtTail("Fourth");
+   linked.insertAt(2, "Fifth");
+   linked.removeFromHead();
+   linked.removeFromTail();
+   linked.removeFrom(1);
+   std::vector<std::string> comp = {"Fifth","Third"};
+   int size = linked.length();
+   REQUIRE(comp == linked.toArray());
+   REQUIRE(size == 2);
+}
+
+TEST_CASE("Search Stuff"){
+   Link<std::string> linked = Link<std::string>("First");
+   linked.insertAtHead("Second");
+   linked.insertAtTail("Third");
+   linked.insertAtTail("Third");
+   linked.insertAt(2, "Fifth");
+   std::vector<std::string> comp = {"Second","Fifth","First","Third", "Third"};
+   int size = linked.length();
+   REQUIRE(comp == linked.toArray());
+   REQUIRE(size == 5);
+   REQUIRE(linked.search("First") == 2);
+   REQUIRE(linked.searchAll("Third") == std::vector<int> {3, 4});
 }
 
  
